e247ed31e57e80014bb922ef05990cdcb04f6b74
wongzh90-arch <wongzh90@gmail.com>
2026-05-09 19:34:29 +0800

    Task: Update contextBuilder.js to include new field

--- src/utils/contextBuilder.js
+++ src/utils/contextBuilder.js
@@ -23,7 +23,7 @@ export class ContextBuilder {
         return [...targetsFirst, ...others].slice(0, maxFiles);
     }
 
-    static buildContext({ targetContents, relatedContents, manifest, tokenBudget = 20000 }) {
+    static buildContext({ targetContents, relatedContents, manifest, tokenBudget = 20000, navText = '' }) {
         let context = '## Target files (full content)\n';
         for (const f of targetContents) {
             context += `\n<file path="${f.path}">\n${f.content}\n</file>\n`;
@@ -36,6 +36,6 @@ export class ContextBuilder {
                 context += `\n<file path="${f.path}" interface-only>\n${interfaceContent}\n</file>\n`;
             }
         }
-        return { contextString: context, tokensUsed: context.length / 4 };
+        return { contextString: context, tokensUsed: context.length / 4, navText };
     }
 }
