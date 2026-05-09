a6d19f172b38c725936c2dd78762b39f918cfe21
wongzh90-arch <wongzh90@gmail.com>
2026-05-09 09:13:26 +0800

    Change React import to namespace import

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { SimpleChat } from './components/SimpleChat.js';
 import { useWorkspaceStore } from './stores/workspaceStore.js';
